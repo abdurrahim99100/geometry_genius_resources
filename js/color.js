c781e771322f774eb4f4d128a590b3fcd7292fb8
cornollc <cornollc@gmail.com>
2023-02-18 22:50:05 +0600

    random color okay

--- js/color.js
+++ js/color.js
@@ -1,13 +1,32 @@
 // random color generat
-function generatColor(){
-    var symbols,color;
-    symbols = '0123456789ABCDEF';
-    color = '#';
-    for(var i = 0; i<6; i++){
-        color = color + symbols[Math.floor(Math.random() * 16)];
-
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
     }
-}
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  
+  document.getElementById('random-color1').addEventListener("mouseover", function(e){
+    e.target.style.backgroundColor = randomColor();
+  });
 
+  document.getElementById('random-color2').addEventListener("mouseover", function(e){
+    e.target.style.backgroundColor = randomColor();
+  });
 
+  document.getElementById('random-color3').addEventListener("mouseover", function(e){
+    e.target.style.backgroundColor = randomColor();
+  });
 
+  document.getElementById('random-color4').addEventListener("mouseover", function(e){
+    e.target.style.backgroundColor = randomColor();
+  });
+
+  document.getElementById('random-color5').addEventListener("mouseover", function(e){
+    e.target.style.backgroundColor = randomColor();
+  });
+
+  document.getElementById('random-color6').addEventListener("mouseover", function(e){
+    e.target.style.backgroundColor = randomColor();
+  });
